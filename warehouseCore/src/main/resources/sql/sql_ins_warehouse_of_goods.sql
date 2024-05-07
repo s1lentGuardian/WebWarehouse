32202d85e06ceb1b6eaea916a5e29a67338f48f0
Skrynnik Rodion <qewbite52@gmail.com>
2024-05-07 10:55:13 +0300

    WH-2 changed warehouse_of_goods inserts

--- warehouseCore/src/main/resources/sql/sql_ins_warehouse_of_goods.sql
+++ warehouseCore/src/main/resources/sql/sql_ins_warehouse_of_goods.sql
@@ -3,4 +3,4 @@ INSERT INTO warehouse_of_goods (warehouse_id, goods_id) VALUES (1, 2);
 INSERT INTO warehouse_of_goods (warehouse_id, goods_id) VALUES (1, 3);
 INSERT INTO warehouse_of_goods (warehouse_id, goods_id) VALUES (1, 4);
 INSERT INTO warehouse_of_goods (warehouse_id, goods_id) VALUES (2, 5);
-INSERT INTO warehouse_of_goods (warehouse_id, goods_id) VALUES (2, 6);
+INSERT INTO warehouse_of_goods (warehouse_id, goods_id) VALUES (2, 2);
