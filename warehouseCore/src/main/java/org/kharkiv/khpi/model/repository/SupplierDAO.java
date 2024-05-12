aeca280436ff3b78e8f2258e12c19882b25b9fdd
Skrynnik Rodion <qewbite52@gmail.com>
2024-05-12 19:29:58 +0300

    WH-3 created findAllSuppliers

--- warehouseCore/src/main/java/org/kharkiv/khpi/model/repository/SupplierDAO.java
+++ warehouseCore/src/main/java/org/kharkiv/khpi/model/repository/SupplierDAO.java
@@ -4,9 +4,14 @@ import jakarta.ejb.Stateless;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.NoResultException;
 import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.TypedQuery;
+import org.eclipse.persistence.internal.sessions.factories.model.platform.SunAS9PlatformConfig;
+import org.kharkiv.khpi.model.Goods;
 import org.kharkiv.khpi.model.Supplier;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -17,6 +22,15 @@ public class SupplierDAO {
 
     @PersistenceContext
     private EntityManager entityManager;
+
+    public List<Supplier> findAllSuppliers() {
+        try {
+            TypedQuery<Supplier> query = entityManager.createQuery("SELECT s FROM Supplier s", Supplier.class);
+            return query.getResultList();
+        } catch (NoResultException e) {
+            return new ArrayList<>();
+        }
+    }
 
     /**
      * Пошук сутності за первинним ключем
