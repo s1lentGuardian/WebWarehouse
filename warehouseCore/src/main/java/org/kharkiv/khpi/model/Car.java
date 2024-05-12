5e510d75adbb957b9e2d00f97da8dc9d8fe9410b
Skrynnik Rodion <qewbite52@gmail.com>
2024-05-12 19:19:29 +0300

    WH-3 added validation and two constructor

--- warehouseCore/src/main/java/org/kharkiv/khpi/model/Car.java
+++ warehouseCore/src/main/java/org/kharkiv/khpi/model/Car.java
@@ -1,10 +1,14 @@
 package org.kharkiv.khpi.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 
 @Entity
 @Table(name = "car")
 public class Car {
+
+    private static final String LICENSE_PLATE_PATTERN = "^[A-Za-z]{2}\\d{4}[A-Za-z]{2}$";
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -17,8 +21,19 @@ public class Car {
     @Column(name = "type_of_car")
     private String typeOfCar;
 
+    @NotEmpty(message = "Licence plate number cannot be empty")
+    @Pattern(regexp = LICENSE_PLATE_PATTERN, message = "License plate number must be in form of AX0000CE")
     @Column(name = "license_plate_number")
     private String licensePlateNumber;
+
+    public Car() {
+    }
+
+    public Car(String make, String typeOfCar, String licensePlateNumber) {
+        this.make = make;
+        this.typeOfCar = typeOfCar;
+        this.licensePlateNumber = licensePlateNumber;
+    }
 
     public Long getId() {
         return carId;
