3f9b237b3084cb8f6053ae78d95d636f657e9516
Skrynnik Rodion <qewbite52@gmail.com>
2024-05-12 19:22:35 +0300

    WH-3 changed attribute in save method to model and created update

--- warehouseCore/src/main/java/org/kharkiv/khpi/model/service/GoodsService.java
+++ warehouseCore/src/main/java/org/kharkiv/khpi/model/service/GoodsService.java
@@ -5,6 +5,7 @@ import jakarta.inject.Inject;
 import org.kharkiv.khpi.model.Goods;
 import org.kharkiv.khpi.model.Supplier;
 import org.kharkiv.khpi.model.repository.GoodsDAO;
+import org.kharkiv.khpi.model.repository.SupplierDAO;
 
 import java.math.BigDecimal;
 import java.util.List;
@@ -15,26 +16,19 @@ public class GoodsService {
     @Inject
     private GoodsDAO goodsDAO;
 
+    @Inject
+    private SupplierDAO supplierDAO;
+
     public List<Goods> findAllGoods() {
         return goodsDAO.findAllGoods();
     }
 
-    public Goods save(String typeOfGoods, String name,  String price, String country, String city, String phoneNumber) {
-        Supplier supplier = createSupplier(country, city, phoneNumber);
-        Goods goods = createGoods(typeOfGoods, name, price);
+    public Goods save(Goods goods, Long supplierId) {
+        Supplier supplier = supplierDAO.findById(supplierId);
 
         goods.setSupplier(supplier);
 
         return goodsDAO.save(goods);
-    }
-
-    public static Supplier createSupplier(String country, String city, String phoneNumber) {
-        Supplier supplier = new Supplier();
-        supplier.setCountry(country);
-        supplier.setCity(city);
-        supplier.setPhoneNumber(phoneNumber);
-
-        return supplier;
     }
 
     private static Goods createGoods(String typeOfGoods, String name,  String price) {
@@ -48,5 +42,18 @@ public class GoodsService {
 
     public void delete(Long id) {
         goodsDAO.delete(id);
+    }
+
+    public void update(Long id, String typeOfGoods, String name,  String price, Long supplierId) {
+        Supplier supplier = supplierDAO.findById(supplierId);
+
+        Goods goods = goodsDAO.findById(id);
+
+        goods.setTypeOfGoods(typeOfGoods);
+        goods.setName(name);
+        goods.setPrice(new BigDecimal(price));
+        goods.setSupplier(supplier);
+
+        goodsDAO.save(goods);
     }
 }
