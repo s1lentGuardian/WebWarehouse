1859042a71c4ede48824405d6ef4a79feaea914f
Skrynnik Rodion <qewbite52@gmail.com>
2024-05-14 23:34:29 +0300

    WH-4 created UserDao

---
+++ warehouseCore/src/main/java/org/kharkiv/khpi/model/repository/UserDao.java
@@ -0,0 +1,51 @@
+package org.kharkiv.khpi.model.repository;
+
+import jakarta.ejb.Stateless;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.NoResultException;
+import jakarta.persistence.PersistenceContext;
+import org.kharkiv.khpi.model.UserEntity;
+import org.kharkiv.khpi.model.exception.WarehouseUserNotFoundException;
+
+@Stateless
+public class UserDao {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    public UserEntity findByUsername(String username) {
+        try {
+            return entityManager.createQuery("SELECT u FROM UserEntity u WHERE u.username = :username", UserEntity.class)
+                    .setParameter("username", username)
+                    .getSingleResult();
+        } catch (NoResultException e) {
+           return null;
+        }
+    }
+
+    public UserEntity save(UserEntity userEntity) {
+        if(userEntity.getUserId() == null)
+            entityManager.persist(userEntity);
+        else
+            entityManager.merge(userEntity);
+
+        return userEntity;
+    }
+
+    public UserEntity findById(long id) {
+        try {
+            return entityManager.find(UserEntity.class, id);
+        } catch (NoResultException e) {
+            throw new WarehouseUserNotFoundException("User with such id doesn't exist");
+        }
+    }
+
+    public void delete(UserEntity userEntity) {
+        entityManager.remove(userEntity);
+    }
+
+    public void delete(long id) {
+        UserEntity userEntity = findById(id);
+        delete(userEntity);
+    }
+}
