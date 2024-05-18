ccb5b3ce6755b6e3e05965261bb32bd09d267c91
Skrynnik Rodion <qewbite52@gmail.com>
2024-05-18 16:57:40 +0300

    WH-4 added ApplicationException annotations for that EJB container return this exception to the client

--- warehouseCore/src/main/java/org/kharkiv/khpi/model/exception/UsernamePasswordEmptyException.java
+++ warehouseCore/src/main/java/org/kharkiv/khpi/model/exception/UsernamePasswordEmptyException.java
@@ -1,5 +1,8 @@
 package org.kharkiv.khpi.model.exception;
 
+import jakarta.ejb.ApplicationException;
+
+@ApplicationException
 public class UsernamePasswordEmptyException extends RuntimeException{
 
     public UsernamePasswordEmptyException() {
