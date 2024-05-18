cfcea29341b7698ea2bd7c047e566f211342ae69
Skrynnik Rodion <qewbite52@gmail.com>
2024-05-18 16:57:51 +0300

    WH-4 added ApplicationException annotations for that EJB container return this exception to the client

--- warehouseCore/src/main/java/org/kharkiv/khpi/model/exception/WarehouseNumberFormatException.java
+++ warehouseCore/src/main/java/org/kharkiv/khpi/model/exception/WarehouseNumberFormatException.java
@@ -1,5 +1,8 @@
 package org.kharkiv.khpi.model.exception;
 
+import jakarta.ejb.ApplicationException;
+
+@ApplicationException
 public class WarehouseNumberFormatException extends RuntimeException{
 
     public WarehouseNumberFormatException(String message) {
