4631aa2532d809f366881b4f4e39cf257cb3ab4a
Skrynnik Rodion <qewbite52@gmail.com>
2024-05-18 16:57:44 +0300

    WH-4 added ApplicationException annotations for that EJB container return this exception to the client

--- warehouseCore/src/main/java/org/kharkiv/khpi/model/exception/UserNotFoundException.java
+++ warehouseCore/src/main/java/org/kharkiv/khpi/model/exception/UserNotFoundException.java
@@ -1,5 +1,8 @@
 package org.kharkiv.khpi.model.exception;
 
+import jakarta.ejb.ApplicationException;
+
+@ApplicationException
 public class UserNotFoundException extends RuntimeException{
 
     public UserNotFoundException(String message) {
